2c9ca2561da74dc348e856e85bfd747d85b8124e
Coobbyo <cooperrutan@gmail.com>
2022-03-21 09:54:05 -0700

    Began Tutorial
    Began Elevation and Terraces up to Part 2 Terraced Edge Connections

--- CatlikeHexMap/Assets/Scripts/HexCell.cs
+++ CatlikeHexMap/Assets/Scripts/HexCell.cs
@@ -3,6 +3,26 @@ public class HexCell : MonoBehaviour
 {
 	public HexCoordinates coordinates;
 	public Color color;
+	public RectTransform uiRect;
+	public int Elevation
+	{
+		get
+		{
+			return elevation;
+		}
+		set
+		{
+			elevation = value;
+			Vector3 position = transform.localPosition;
+			position.y = value * HexMetrics.elevationStep;
+			transform.localPosition = position;
+
+			Vector3 uiPosition = uiRect.localPosition;
+			uiPosition.z = elevation * -HexMetrics.elevationStep;
+			uiRect.localPosition = uiPosition;
+		}
+	}
+	private int elevation;
 
 	[SerializeField] private HexCell[] neighbors;
 
