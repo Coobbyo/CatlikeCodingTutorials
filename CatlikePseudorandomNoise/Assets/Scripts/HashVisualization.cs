9bd71c17d42bb704f80113ff51c9b349149b6c2e
Coobbyo <cooperrutan@gmail.com>
2022-03-21 19:11:36 -0700

    Continued Hex Map Tutorial
    Up to 3.4 Vectorized Shapes Job

--- CatlikePseudorandomNoise/Assets/Scripts/HashVisualization.cs
+++ CatlikePseudorandomNoise/Assets/Scripts/HashVisualization.cs
@@ -11,64 +11,84 @@ public class HashVisualization : MonoBehaviour
 	[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
 	private struct HashJob : IJobFor
     {
-        public int resolution;
-		public float invResolution;
-        public SmallXXHash hash;
-
-		[WriteOnly] public NativeArray<uint> hashes;
+		[ReadOnly] public NativeArray<float3x4> positions;
+		[WriteOnly] public NativeArray<uint4> hashes;
         
+		public SmallXXHash4 hash;
+		public float3x4 domainTRS;
+
+		float4x3 TransformPositions (float3x4 trs, float4x3 p) => float4x3(
+			trs.c0.x * p.c0 + trs.c1.x * p.c1 + trs.c2.x * p.c2 + trs.c3.x,
+			trs.c0.y * p.c0 + trs.c1.y * p.c1 + trs.c2.y * p.c2 + trs.c3.y,
+			trs.c0.z * p.c0 + trs.c1.z * p.c1 + trs.c2.z * p.c2 + trs.c3.z
+		);
+
 		public void Execute(int i)
         {
-			int v = (int)floor(invResolution * i + 0.00001f);
-			int u = i - resolution * v - resolution / 2;
-			v -= resolution / 2;
+			float4x3 p = TransformPositions(domainTRS, transpose(positions[i]));
 
-			hashes[i] = hash.Eat(u).Eat(v);
+			int4 u = (int4)floor(p.c0);
+			int4 v = (int4)floor(p.c1);
+			int4 w = (int4)floor(p.c2);
+
+			hashes[i] = hash.Eat(u).Eat(v).Eat(w);
 		}
 	}
 
     private static int
 		hashesId = Shader.PropertyToID("_Hashes"),
+		positionsId = Shader.PropertyToID("_Positions"),
+		normalsId = Shader.PropertyToID("_Normals"),
 		configId = Shader.PropertyToID("_Config");
     
 	[SerializeField] private Mesh instanceMesh;
 	[SerializeField] private Material material;
 	[SerializeField, Range(1, 512)] private int resolution = 16;
-    [SerializeField, Range(-2f, 2f)] private float verticalOffset = 1f;
+    [SerializeField, Range(-0.5f, 0.5f)] private float displacement = 0.1f;
     [SerializeField] private int seed;
+	[SerializeField] private SpaceTRS domain = new SpaceTRS
+	{
+		scale = 8f
+	};
 
 	private NativeArray<uint> hashes;
-	private ComputeBuffer hashesBuffer;
+	private NativeArray<float3> positions, normals;
+	private ComputeBuffer hashesBuffer, positionsBuffer, normalsBuffer;
 	private MaterialPropertyBlock propertyBlock;
+	private bool isDirty;
+	private Bounds bounds;
 
     void OnEnable()
     {
+		isDirty = true;
 		int length = resolution * resolution;
 		hashes = new NativeArray<uint>(length, Allocator.Persistent);
+		positions = new NativeArray<float3>(length, Allocator.Persistent);
+		normals = new NativeArray<float3>(length, Allocator.Persistent);
 		hashesBuffer = new ComputeBuffer(length, 4);
-
-		new HashJob
-        {
-			hashes = hashes,
-			resolution = resolution,
-			invResolution = 1f / resolution,
-            hash = SmallXXHash.Seed(seed)
-		}.ScheduleParallel(hashes.Length, resolution, default).Complete();
-
-		hashesBuffer.SetData(hashes);
+		positionsBuffer = new ComputeBuffer(length, 3 * 4);
+		normalsBuffer = new ComputeBuffer(length, 3 * 4);
 
 		propertyBlock ??= new MaterialPropertyBlock();
 		propertyBlock.SetBuffer(hashesId, hashesBuffer);
+		propertyBlock.SetBuffer(positionsId, positionsBuffer);
+		propertyBlock.SetBuffer(normalsId, normalsBuffer);
 		propertyBlock.SetVector(configId, new Vector4(
-            resolution, 1f / resolution, verticalOffset / resolution
+            resolution, 1f / resolution, displacement
         ));
 	}
 
     void OnDisable()
     {
 		hashes.Dispose();
+		positions.Dispose();
+		normals.Dispose();
 		hashesBuffer.Release();
+		positionsBuffer.Release();
+		normalsBuffer.Release();
 		hashesBuffer = null;
+		positionsBuffer = null;
+		normalsBuffer = null;
 	}
 
 	void OnValidate()
@@ -82,9 +102,35 @@ public class HashVisualization : MonoBehaviour
 
     void Update()
     {
+		if(isDirty || transform.hasChanged)
+		{
+			isDirty = false;
+			transform.hasChanged = false;
+
+			JobHandle handle = Shapes.Job.ScheduleParallel(
+				positions, normals, resolution, transform.localToWorldMatrix, default
+			);
+
+			new HashJob
+			{
+				positions = positions.Reinterpret<float3x4>(3 * 4),
+				hashes = hashes.Reinterpret<uint4>(4),
+				hash = SmallXXHash.Seed(seed),
+				domainTRS = domain.Matrix
+			}.ScheduleParallel(hashes.Length / 4, resolution, handle).Complete();
+
+			hashesBuffer.SetData(hashes);
+			positionsBuffer.SetData(positions);
+			normalsBuffer.SetData(normals);
+
+			bounds = new Bounds(
+				transform.position,
+				float3(2f * cmax(abs(transform.lossyScale)) + displacement)
+			);
+		}
+
 		Graphics.DrawMeshInstancedProcedural(
-			instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
-			hashes.Length, propertyBlock
+			instanceMesh, 0, material, bounds, hashes.Length, propertyBlock
 		);
 	}
 }
--- CatlikePseudorandomNoise/Assets/Scripts/SmallXXHash.cs
+++ CatlikePseudorandomNoise/Assets/Scripts/SmallXXHash.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics;
+
 public readonly struct SmallXXHash
 {
 	private const uint primeA = 0b10011110001101110111100110110001;
@@ -14,6 +16,9 @@ public readonly struct SmallXXHash
 	}
 
     public static SmallXXHash Seed (int seed) => (uint)seed + primeE;
+
+	public static implicit operator SmallXXHash4 (SmallXXHash hash) =>
+		new SmallXXHash4(hash.accumulator);
 
     public static implicit operator SmallXXHash (uint accumulator) =>
 		new SmallXXHash(accumulator);
@@ -35,5 +40,41 @@ public readonly struct SmallXXHash
 		RotateLeft(accumulator + data * primeE, 11) * primeA;
 
     static uint RotateLeft (uint data, int steps) =>
+		(data << steps) | (data >> 32 - steps);
+}
+
+public readonly struct SmallXXHash4
+{
+	private const uint primeB = 0b10000101111010111100101001110111;
+	private const uint primeC = 0b11000010101100101010111000111101;
+	private const uint primeD = 0b00100111110101001110101100101111;
+	private const uint primeE = 0b00010110010101100110011110110001;
+
+    private readonly uint4 accumulator;
+
+	public SmallXXHash4(uint4 accumulator)
+    {
+		this.accumulator = accumulator;
+	}
+
+    public static SmallXXHash4 Seed (int4 seed) => (uint4)seed + primeE;
+
+    public static implicit operator SmallXXHash4 (uint4 accumulator) =>
+		new SmallXXHash4(accumulator);
+
+    public static implicit operator uint4 (SmallXXHash4 hash) {
+		uint4 avalanche = hash.accumulator;
+		avalanche ^= avalanche >> 15;
+		avalanche *= primeB;
+		avalanche ^= avalanche >> 13;
+		avalanche *= primeC;
+		avalanche ^= avalanche >> 16;
+		return avalanche;
+	}
+
+    public SmallXXHash4 Eat (int4 data) =>
+		RotateLeft(accumulator + (uint4)data * primeC, 17) * primeD;
+
+    static uint4 RotateLeft (uint4 data, int steps) =>
 		(data << steps) | (data >> 32 - steps);
 }
