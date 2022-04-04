a135028304fbc9557441cf384ade6f3a8e15b776
Coobbyo <cooperrutan@gmail.com>
2022-04-03 19:22:16 -0700

    Began Tutorial
    Began Procedural Meshes: Square Grid
    Up to Part 1.6 Generating a Quad

---
+++ CatlikeProceduralMeshes/Assets/Scripts/Precedural Meshes/SquareGrid.cs
@@ -0,0 +1,16 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+using static Unity.Mathematics.math;
+
+namespace ProceduralMeshes.Generators
+{
+	public struct SquareGrid : IMeshGenerator
+    {
+        public int VertexCount => 0;
+		public int IndexCount => 0;
+		public int JobLength => 0;
+
+		public void Execute<S> (int z, S streams) where S : struct, IMeshStreams {}
+    }
+}
