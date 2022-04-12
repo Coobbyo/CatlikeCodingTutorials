79e404a92c542ee9f1674e297ef5cc7c29e4fb78
Coobbyo <cooperrutan@gmail.com>
2022-04-12 08:58:24 -0700

    Began Tutorial
    Began Procedural Meshes: UV Sphere
    Up to Part 2.2 Cylinder

--- CatlikeProceduralMeshes/Assets/Scripts/ProceduralMesh.cs
+++ CatlikeProceduralMeshes/Assets/Scripts/ProceduralMesh.cs
@@ -7,23 +7,30 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
-	private static MeshJobScheduleDelegate[] jobs = {
+	private static MeshJobScheduleDelegate[] jobs =
+	{
 		MeshJob<SquareGrid, SingleStream>.ScheduleParallel,
 		MeshJob<SharedSquareGrid, SingleStream>.ScheduleParallel,
 		MeshJob<SharedTriangleGrid, SingleStream>.ScheduleParallel,
 		MeshJob<PointyHexagonGrid, SingleStream>.ScheduleParallel,
-		MeshJob<FlatHexagonGrid, SingleStream>.ScheduleParallel
+		MeshJob<FlatHexagonGrid, SingleStream>.ScheduleParallel,
+		MeshJob<UVSphere, SingleStream>.ScheduleParallel
 	};
 
 	public enum MeshType
 	{
-		SquareGrid, SharedSquareGrid, SharedTriangleGrid, PointyHexagonGrid, FlatHexagonGrid
+		SquareGrid, SharedSquareGrid, SharedTriangleGrid,
+		FlatHexagonGrid, PointyHexagonGrid, UVSphere
 	};
+	[System.Flags] public enum GizmoMode { Nothing = 0, Vertices = 1, Normals = 0b10, Tangents = 0b100 }
 
 	[SerializeField] private MeshType meshType;
-
+	[SerializeField] private GizmoMode gizmos;
 	[SerializeField, Range(1, 50)] private int resolution = 1;
+
     private Mesh mesh;
+	private Vector3[] vertices, normals;
+	private Vector4[] tangents;
 
 	private void Awake()
     {
@@ -40,6 +47,10 @@ public class ProceduralMesh : MonoBehaviour
 	{
 		GenerateMesh();
 		enabled = false;
+
+		vertices = null;
+		normals = null;
+		tangents = null;
 	}
 	
 	private void GenerateMesh()
@@ -51,4 +62,48 @@ public class ProceduralMesh : MonoBehaviour
 
 		Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
     }
+
+	void OnDrawGizmos()
+	{
+		if(gizmos == GizmoMode.Nothing || mesh == null)
+			return;
+
+		bool drawVertices = (gizmos & GizmoMode.Vertices) != 0;
+		bool drawNormals = (gizmos & GizmoMode.Normals) != 0;
+		bool drawTangents = (gizmos & GizmoMode.Tangents) != 0;
+
+		if (vertices == null)
+		{
+			vertices = mesh.vertices;
+		}
+		if(drawNormals && normals == null)
+		{
+			normals = mesh.normals;
+		}
+		if(drawTangents && tangents == null)
+		{
+			tangents = mesh.tangents;
+		}
+
+		Transform t = transform;
+		for(int i = 0; i < vertices.Length; i++)
+		{
+			Vector3 position = t.TransformPoint(vertices[i]);
+			if (drawVertices)
+			{
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawSphere(position, 0.02f);
+			}
+			if (drawNormals)
+			{
+				Gizmos.color = Color.green;
+				Gizmos.DrawRay(position, t.TransformDirection(normals[i]) * 0.2f);
+			}
+			if (drawTangents)
+			{
+				Gizmos.color = Color.red;
+				Gizmos.DrawRay(position, t.TransformDirection(tangents[i]) * 0.2f);
+			}
+		}
+	}
 }
