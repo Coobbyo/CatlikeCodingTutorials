50649ed703ea5f857ff0ec4f15a8ff77c04d43d7
Coobbyo <cooperrutan@gmail.com>
2022-04-20 08:52:49 -0700

    Completed Tutorial
    Completed Object Management: Persisting Objects

---
+++ CatlikeObjectManagement/Assets/Scripts/PersistentStorage.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using UnityEngine;
+
+public class PersistentStorage : MonoBehaviour
+{
+	string savePath;
+
+	void Awake()
+    {
+        Debug.Log(Application.persistentDataPath);
+		savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+	}
+
+	public void Save(PersistableObject o)
+    {
+		using(var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+			o.Save(new GameDataWriter(writer));
+		}
+	}
+
+	public void Load(PersistableObject o)
+    {
+		using (var reader = new BinaryReader(File.Open(savePath, FileMode.Open)))
+        {
+			o.Load(new GameDataReader(reader));
+		}
+	}
+}
