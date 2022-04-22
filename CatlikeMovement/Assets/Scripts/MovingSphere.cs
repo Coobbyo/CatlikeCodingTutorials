d4be534fa3ed064c54c6db8bd716ec70e2e89cb4
Coobbyo <cooperrutan@gmail.com>
2022-04-22 16:23:04 -0700

    Finished Tutorial
    Finished Movement: Physics
    NOTE: There is a bug wit the balls rolling downhill if they are under the maxGroundAngle...but I don't know what it is...

--- CatlikeMovement/Assets/Scripts/MovingSphere.cs
+++ CatlikeMovement/Assets/Scripts/MovingSphere.cs
@@ -6,17 +6,22 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f, maxAirAcceleration = 1f;
 	[SerializeField, Range(0f, 10f)] private float jumpHeight = 2f;
 	[SerializeField, Range(0, 5)] private int maxAirJumps = 0;
+	[SerializeField, Range(0f, 90f)] private float maxGroundAngle = 25f;
 
     private Vector3 velocity, desiredVelocity;
 	private Rigidbody body;
 	private bool desiredJump;
-	private bool onGround;
+	private int groundContactCount;
+	private bool OnGround => groundContactCount > 0;
 	private int jumpPhase;
+	private float minGroundDotProduct;
+	private Vector3 contactNormal;
 
 
 	private void Awake()
 	{
 		body = GetComponent<Rigidbody>();
+		OnValidate();
 	}
 
     private void Update()
@@ -35,29 +40,32 @@ public class MovingSphere : MonoBehaviour
 	private void FixedUpdate()
 	{
 		UpdateState();
-		float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-		float maxSpeedChange = acceleration * Time.deltaTime;
-		velocity.x =
-			Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-		velocity.z =
-			Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+		AdjustVelocity();
 
-		if (desiredJump)
+		if(desiredJump)
 		{
 			desiredJump = false;
 			Jump();
 		}
 
 		body.velocity = velocity;
-		onGround = false;
+		ClearState();
 	}
 
 	void UpdateState()
 	{
 		velocity = body.velocity;
-		if(onGround)
+		if(OnGround)
 		{
 			jumpPhase = 0;
+			if(groundContactCount > 1)
+			{
+				contactNormal.Normalize();
+			}
+		}
+		else
+		{
+			contactNormal = Vector3.up;
 		}
 	}
 
@@ -76,22 +84,62 @@ public class MovingSphere : MonoBehaviour
 		for(int i = 0; i < collision.contactCount; i++)
 		{
 			Vector3 normal = collision.GetContact(i).normal;
-			onGround |= normal.y >= 0.9f;
+			if(normal.y >= minGroundDotProduct)
+			{
+				groundContactCount += 1;
+				contactNormal += normal;
+			}
 
 		}
 	}
 
+	private void ClearState()
+	{
+		groundContactCount = 0;
+		contactNormal = Vector3.zero;
+	}
+
+	private void OnValidate()
+	{
+		minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+	}
+
 	private void Jump()
 	{
-		if(onGround || jumpPhase < maxAirJumps)
+		if(OnGround || jumpPhase < maxAirJumps)
 		{
 			jumpPhase += 1;
 			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-			if(velocity.y > 0f)
+			float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+			if(alignedSpeed > 0f)
 			{
-				jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+				jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
 			}
-			velocity.y += jumpSpeed;
+			velocity += contactNormal * jumpSpeed;
 		}
+	}
+
+	private Vector3 ProjectOnContactPlane(Vector3 vector)
+	{
+		return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+	}
+
+	private void AdjustVelocity()
+	{
+		Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+		Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+		float currentX = Vector3.Dot(velocity, xAxis);
+		float currentZ = Vector3.Dot(velocity, zAxis);
+
+		float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+		float maxSpeedChange = acceleration * Time.deltaTime;
+
+		float newX =
+			Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+		float newZ =
+			Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+		velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
 	}
 }
