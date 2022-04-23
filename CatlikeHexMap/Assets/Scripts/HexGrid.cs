aa19c3748556a61c13b77a3c7168f40675b44175
Coobbyo <cooperrutan@gmail.com>
2022-04-23 13:07:35 -0700

    Completed Tutorial
    Completed Hex Map: Animating Movement

--- CatlikeHexMap/Assets/Scripts/HexGrid.cs
+++ CatlikeHexMap/Assets/Scripts/HexGrid.cs
@@ -227,7 +227,7 @@ public class HexGrid : MonoBehaviour
 				return true;
 			}
 
-			int currentTurn = current.Distance / speed;
+			int currentTurn = (current.Distance - 1) / speed;
 
 			for(HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
 			{
@@ -258,7 +258,7 @@ public class HexGrid : MonoBehaviour
 				}
 
 				int distance = current.Distance + moveCost;
-				int turn = distance / speed;
+				int turn = (distance - 1) / speed;
 				if(turn > currentTurn)
 				{
 					distance = turn * speed + moveCost;
@@ -293,7 +293,7 @@ public class HexGrid : MonoBehaviour
 			HexCell current = currentPathTo;
 			while(current != currentPathFrom)
 			{
-				int turn = current.Distance / speed;
+				int turn = (current.Distance - 1) / speed;
 				current.SetLabel(turn.ToString());
 				current.EnableHighlight(Color.white);
 				current = current.PathFrom;
@@ -301,6 +301,21 @@ public class HexGrid : MonoBehaviour
 		}
 		currentPathFrom.EnableHighlight(Color.blue);
 		currentPathTo.EnableHighlight(Color.red);
+	}
+
+	public List<HexCell> GetPath()
+	{
+		if(!currentPathExists)
+			return null;
+
+		List<HexCell> path = ListPool<HexCell>.Get();
+		for(HexCell c = currentPathTo; c != currentPathFrom; c = c.PathFrom)
+		{
+			path.Add(c);
+		}
+		path.Add(currentPathFrom);
+		path.Reverse();
+		return path;
 	}
 
 	public void ClearPath()
--- CatlikeHexMap/Assets/Scripts/UI/HexGameUI.cs
+++ CatlikeHexMap/Assets/Scripts/UI/HexGameUI.cs
@@ -60,7 +60,7 @@ public class HexGameUI : MonoBehaviour
     {
 		if(grid.HasPath)
         {
-			selectedUnit.Location = currentCell;
+			selectedUnit.Travel(grid.GetPath());
 			grid.ClearPath();
 		}
 	}
