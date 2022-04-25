67404283e8a4c27299862c5c701de107ad9469a6
Coobbyo <cooperrutan@gmail.com>
2022-04-25 07:39:12 -0700

    Finished Tutorial
    Finished Object Management: Fabricating Shapes

--- CatlikeObjectManagement/Assets/Scripts/GameDataReader.cs
+++ CatlikeObjectManagement/Assets/Scripts/GameDataReader.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class GameDataReader
 {
-	BinaryReader reader;
+	public int Version { get; }
 
-	public GameDataReader(BinaryReader reader)
+	private BinaryReader reader;
+
+	public GameDataReader(BinaryReader reader, int version)
     {
 		this.reader = reader;
+		this.Version = version;
 	}
 
     public float ReadFloat()
@@ -36,6 +39,16 @@ public class GameDataReader
 		value.x = reader.ReadSingle();
 		value.y = reader.ReadSingle();
 		value.z = reader.ReadSingle();
+		return value;
+	}
+
+	public Color ReadColor()
+	{
+		Color value;
+		value.r = reader.ReadSingle();
+		value.g = reader.ReadSingle();
+		value.b = reader.ReadSingle();
+		value.a = reader.ReadSingle();
 		return value;
 	}
 }
--- CatlikeObjectManagement/Assets/Scripts/Shape.cs
+++ CatlikeObjectManagement/Assets/Scripts/Shape.cs
@@ -20,6 +20,46 @@ public class Shape : PersistableObject
 			}
         }
     }
+    public int MaterialId { get; private set; }
 
+	private static int colorPropertyId = Shader.PropertyToID("_Color");
+	private static MaterialPropertyBlock sharedPropertyBlock;
     private int shapeId = int.MinValue;
+	private Color color;
+	private MeshRenderer meshRenderer;
+
+
+	private void Awake()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+	}
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        meshRenderer.material = material;
+		MaterialId = materialId;
+    }
+
+	public void SetColor(Color color)
+	{
+		this.color = color;
+		if(sharedPropertyBlock == null)
+		{
+			sharedPropertyBlock = new MaterialPropertyBlock();
+		}
+		sharedPropertyBlock.SetColor(colorPropertyId, color);
+		meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+	}
+
+	public override void Save(GameDataWriter writer)
+	{
+		base.Save(writer);
+		writer.Write(color);
+	}
+
+	public override void Load(GameDataReader reader)
+	{
+		base.Load(reader);
+		SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+	}
 }
